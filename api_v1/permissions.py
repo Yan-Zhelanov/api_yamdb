2990ea5d8a4f4c9a5e7ae85f65cb5a355891aa2b
Kenshin-Him <shakhabkhan1916@gmail.com>
2021-06-14 10:43:45 +0300

    added_views

---
+++ api_v1/permissions.py
@@ -0,0 +1,26 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdmin(BasePermission):
+
+    def has_permission(self, request, view):
+        return (request.user and
+                request.user.is_authenticated and
+                request.user.is_active and
+                request.user.is_admin)
+
+    def has_object_permission(self, request, view, obj):
+        return (request.user and
+                request.user.is_authenticated and
+                request.user.is_active and
+                request.user.is_authenticated and
+                request.user.is_admin)
+
+
+class ReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+
+    def has_object_permission(self, request, view, obj):
+        return request.method in SAFE_METHODS
---
+++ api_v1/views.py
@@ -0,0 +1,48 @@
+from django.db.models import Avg
+from django_filters.rest_framework.backends import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.mixins import (CreateModelMixin, DestroyModelMixin,
+                                   ListModelMixin)
+from rest_framework.permissions import SAFE_METHODS
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from .filters import TitleFilter
+from .models import Title, Genre, Category
+from .permissions import ReadOnly, IsAdmin
+from .serializers import (TitlesSerializerGet, TitlesSerializerPost,
+                          GenresSerializer, CategoriesSerializer)
+
+
+class CreateDelListViewset(CreateModelMixin, DestroyModelMixin,
+                           ListModelMixin, GenericViewSet):
+    pass
+
+
+class CategoriesViewSet(CreateDelListViewset):
+    queryset = Category.objects,all()
+    serializer_class = CategoriesSerializer
+    permission_classes = (IsAdmin|ReadOnly,)
+    lookup_field = 'slug'
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
+
+
+class GenresViewSet(CreateDelListViewset):
+    queryset = Genre.objects.all()
+    serializer_class = GenresSerializer
+    permission_classes = (IsAdmin|ReadOnly,)
+    lookup_field = 'slug'
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
+
+
+class TitlesViewSet(ModelViewSet):
+    permission_classes = (IsAdmin|ReadOnly,)
+    queryset = Title.objects.annotate(
+        rating=Avg('reviews__score')).order_by('name')
+    filterset_class = TitleFilter
+    filter_backends = (DjangoFilterBackend,)
+
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+            return TitlesSerializerGet
+        return TitlesSerializerPost
