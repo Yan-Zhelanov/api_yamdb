ef765f61689594b28a1226b005761f852d6bfe4c
Yan-Zhelanov <yzhelanov@gmail.com>
2021-06-15 22:00:20 +0300

    v0.1 — Fixed settings.py

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -36,7 +36,6 @@ ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
 # Application definition
 
 INSTALLED_APPS = [
-    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
