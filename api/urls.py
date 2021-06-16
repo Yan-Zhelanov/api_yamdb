0ca70f9f99628c71d39447066e6fcb15fd0da3cc
Kenshin-Him <shakhabkhan1916@gmail.com>
2021-06-16 13:33:02 +0300

    .

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,35 @@
-from .views import TitlesViewset, GenresViewSet, CategoriesViewSet
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(
+from .views import UserViewSet, SendEmail, GetToken, CommentViewSet, ReviewViewSet, TitlesViewset, GenresViewSet, CategoriesViewSet
+
+router_v1 = DefaultRouter()
+router_v1.register(r'users', UserViewSet, basename='users')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+router_v1.register(
     r'titles',
     TitlesViewset,
     basename='titles')
-router.register(
+router_v1.register(
     r'categories',
     CategoriesViewSet,
     basename='categories')
-router.register(
+router_v1.register(
     r'genres',
     GenresViewSet,
     basename='genres')
+
+urlpatterns = [
+    path('v1/auth/email/', SendEmail.as_view(), name='send_email'),
+    path('v1/auth/token/', GetToken.as_view(), name='get_token'),
+    path('v1/', include(router_v1.urls)),
+]
