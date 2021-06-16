3a284f23566f6c295d39971b057834fcb393ca2b
Kenshin-Him <shakhabkhan1916@gmail.com>
2021-06-16 11:40:51 +0300

    ..

--- api/validators.py
+++ api/validators.py
@@ -4,6 +4,6 @@ from django.core.exceptions import ValidationError
 def custom_year_validator(value):
     if value > datetime.now().year:
         raise ValidationError(
-            ('Year cant be greater then current.'),
+            ('Год не может быт больше текущего года.'),
             params={'value': value},
         )
