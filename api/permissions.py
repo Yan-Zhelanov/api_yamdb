7edf8c3bbbbbd80f079d010cc554c6d9ec5e68cb
Yan-Zhelanov <yzhelanov@gmail.com>
2021-06-21 15:07:27 +0300

    v1.1 — Fixed max_length for CustomUser confirmation code and role, add constants for roles, added to all permissions access for staff and fixed method in UserViewSet for update roles.

--- api/permissions.py
+++ api/permissions.py
@@ -1,31 +1,36 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
+from users.roles import USER, MODERATOR, ADMIN
+
 
 class IsAdminOrMe(BasePermission):
     def has_permission(self, request, view):
-        return (view.kwargs.get('pk', None) == 'me'
-                or request.user.is_superuser
-                or request.user.role == 'admin')
-
-
-class IsAuthenticatedOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or request.user.is_authenticated)
+        return (
+            view.kwargs.get('pk') == 'me'
+            or request.user.role == ADMIN
+            or request.user.is_staff
+            or request.user.is_superuser
+        )
 
 
 class IsAuthorOrModeratorOrAdminOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS
-                or request.user == obj.author
-                or request.user.role == 'moderator'
-                or request.user.role == 'admin'
-                or request.user.is_superuser)
+        return (
+            request.method in SAFE_METHODS
+            or request.user == obj.author
+            or request.user.role == MODERATOR
+            or request.user.role == ADMIN
+            or request.user.is_staff
+            or request.user.is_superuser
+        )
 
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or (request.user.is_authenticated
-                    and (request.user.is_superuser
-                         or request.user.role == 'admin')))
+        return (
+            request.method in SAFE_METHODS
+            or (request.user.is_authenticated
+                and (request.user.role == ADMIN
+                     or request.user.is_staff
+                     or request.user.is_superuser))
+        )
---
+++ users/roles.py
@@ -0,0 +1,3 @@
+USER = 'user'
+MODERATOR = 'moderator'
+ADMIN = 'admin'
